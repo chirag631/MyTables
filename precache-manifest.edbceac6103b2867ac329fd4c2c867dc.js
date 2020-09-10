642be498f12e696fb629da5036ec3b03c00e9c45
chirag631 <05042000cdk@gmail.com>
2020-09-10 13:15:29 +0530

    Updates

---
+++ precache-manifest.edbceac6103b2867ac329fd4c2c867dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b6322fc8fecc7a462b6b2f119b4672",
+    "url": "/MyTables/index.html"
+  },
+  {
+    "revision": "73370ca4557907c4d989",
+    "url": "/MyTables/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cc99bf25a4cf0a72f920",
+    "url": "/MyTables/static/css/main.66ddf15b.chunk.css"
+  },
+  {
+    "revision": "73370ca4557907c4d989",
+    "url": "/MyTables/static/js/2.94bb7456.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/MyTables/static/js/2.94bb7456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc99bf25a4cf0a72f920",
+    "url": "/MyTables/static/js/main.f92001d4.chunk.js"
+  },
+  {
+    "revision": "559402a631757ebd5b59",
+    "url": "/MyTables/static/js/runtime-main.ad67d4c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MyTables/static/media/logo.5d5d9eef.svg"
+  }
+]);
